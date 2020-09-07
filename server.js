5b2f2f5f5b0249dc43cb5e9ec5f8344f7f8f750e
Ncr96 <46475363+Ncr96@users.noreply.github.com>
2020-09-07 10:04:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const monk = require('monk');
+const db = monk('localhost/database');
+const people = db.get('people');
+app.use(express.json());
+app.listen(3000,()=>{console.log('listening on http://localhost:3000')});
+app.use(express.static('public',{extensions:['html','htm']}));
+
+app.post('/data',(req,res)=>{
+    const data={
+        name:req.body.name,
+        time:req.body.time
+    };
+    console.log(req.body);
+   people.insert(data).then(createdata=>{
+       res.json(createdata);
+   });
+});
+
+app.get('/data',(req,res)=>{
+  people.find().then(data=>res.json(data));
+});
+
+app.put('/update',(req,res)=>{
+    people.findOneAndUpdate({name:req.body.name,time:req.body.time},{$set:{name:req.body.newname,time:req.body.newtime}}).then(updatedDoc=>{});
+    console.log("updated");
+    var data = req.body;
+    res.json(data);
+});
+
+app.put('/delete',(req,res)=>{
+    people.remove({name:req.body.name});
+    console.log("Deleted");
+    var data = req.body;
+    res.json(data);
+});
+
+
+app.get('/view/:name',(req,res)=>{
+    var getname = req.params.name;
+    console.log(getname);
+    people.find({name:getname}).then(data => res.json(data));
+});
